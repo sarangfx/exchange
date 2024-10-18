ff1f7d127e23563fc7f2ce997653cf1da763372d
saravanan <saran.gfx@gmail.com>
2024-10-19 00:08:17 +0530

    undo

--- src/index.jsx
+++ src/index.jsx
@@ -31,8 +31,13 @@ import '@fontsource/public-sans/700.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// ==============================|| MAIN - REACT DOM RENDER ||============================== //
+
+root.render(<App />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
