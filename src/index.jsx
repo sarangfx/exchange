0fc1ca197fff327162a3a4b8cd566017a0ab3fdf
saravanan <saran.gfx@gmail.com>
2024-10-19 21:23:00 +0530

    undo webvitals

--- src/index.jsx
+++ src/index.jsx
@@ -29,6 +29,7 @@ import '@fontsource/public-sans/700.css';
 
 // project import
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -39,3 +40,4 @@ root.render(<App />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
